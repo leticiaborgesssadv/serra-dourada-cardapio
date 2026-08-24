87a9d86ca48d4f57f6bf073c8bb7837c5440c29d
leticiaborgesssadv <leticiaborgesssadv@gmail.com>
2026-08-24 09:14:07 -0300

    Bump cache version para 23

--- gestao/app.js
+++ gestao/app.js
@@ -5,7 +5,7 @@ import { sb } from "./lib/supabase.js";
 
 const html = htm.bind(h);
 
-const VERSAO_CACHE = "22";
+const VERSAO_CACHE = "23";
 
 const MODULOS = [
   { rota: "financeiro", rotulo: "Financeiro", pronto: true },
